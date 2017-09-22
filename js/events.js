91fe5cf5067d2ff9307104b2f31b199efea0ae18
Anthony Grimes <agrimes@charter.net>
2017-09-22 19:58:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,21 @@
 //define functions here
+function getIt () {
+    $('p').on('click', function () {
+        document.alert('Hey!');
+    });
+}
+
+function frameIt () {
+
+}
+
+function pressIt () {
+
+}
+
+function submitIt () {
+
+}
 
 $(document).ready(function(){
 
