d10a7a0d1ffce67856956ffed688f6ef0399ef67
Anthony Grimes <agrimes@charter.net>
2017-09-22 20:14:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt () {
 
 function submitIt () {
     $('form').on('submit', function () {
-        
+        alert('Your form is going to be submitted now.');
     });
 }
 
@@ -31,5 +31,5 @@ $(document).ready(function(){
 getIt();
 frameIt();
 pressIt();
-
+submitIt();
 });
